c14e39df8025f6028793fcd11e72fba8969cf286
Yash Dubey <yashdubey2285@gmail.com>
2026-01-22 08:44:19 +0530

    feat: add user registration controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,26 @@
+const User = require("../models/user.model");
+
 function test(req, res) {
   res.json({
     message: "auth route reached",
   });
+}
+
+async function registerUser(req, res) {
+  const { name, email, password } = req.body;
+
+  if (!(name && email && password))
+    return res.status(400).json({ message: "all fields are required" });
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser)
+    return res.status(409).json({ message: "user already exists" });
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  res.status(201).json({ message: "user registered successfully" });
 }
 
 module.exports = test;
